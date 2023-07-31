6729d671fffdb00d469143fc270ca617f0cd519a
Atoche Pascual <jmatochepascual@gmail.com>
2023-07-31 12:20:17 -1000

    [FEATURE] error compoenent

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className="mb-6 p-3 text-center font-bold text-white uppercase bg-red-800">Todos los campos son obligatorios</div>
+  )
+}
+
+export default Error
