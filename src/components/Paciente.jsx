e1621e125f9ee98bd765e4eafe5970a8deb34655
Atoche Pascual <jmatochepascual@gmail.com>
2023-07-31 10:09:50 -1000

    [FEATURE] paciente component

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+function Paciente() {
+  return (
+    <div className='mb-5 py-10 px-5 rounded-lg shadow-lg bg-white'>
+      <p className='mb-2 text-gray-700 font-bold uppercase'>Nombre: <span className='font-normal normal-case'>Hook</span></p>
+      <p className='mb-2 text-gray-700 font-bold uppercase'>Propietario: <span className='font-normal normal-case'>jeancarlos</span></p>
+      <p className='mb-2 text-gray-700 font-bold uppercase'>emil: <span className='font-normal normal-case'>correo@correo.com</span></p>
+      <p className='mb-2 text-gray-700 font-bold uppercase'>fecha alta: <span className='font-normal normal-case'>20-5-2024</span></p>
+      <p className='mb-2 text-gray-700 font-bold uppercase'>síntomas: <span className='font-normal normal-case'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis, doloribus.</span></p>
+    </div>
+  )
+}
+
+export default Paciente
