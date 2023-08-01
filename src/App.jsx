a170eade65a8bf655c08f273cec3466967def6f3
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-01 10:32:52 -1000

    [FEATURE] sincronizar localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) || []);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     const pacientesActualizado = pacientes.filter(paciente => paciente.id !== id);
