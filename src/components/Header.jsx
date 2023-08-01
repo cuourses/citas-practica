b785e63ad8748e0b0445b7b86609182bedc8eee0
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-01 12:11:35 -1000

    [FEATURE] cambiar titulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <h1 className="mb-12 text-5xl font-black text-center text-zinc-900">Seguimiento Pacientes
+    <h1 className="mb-12 text-5xl font-black text-center text-zinc-900">Seguimiento Pacientes con GitHub
       <span className="block text-indigo-600">Veterinaria</span>
     </h1>
   )
